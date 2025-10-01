a248e24a030b487c191b2e8b2a7e3980768bb289
Victor Basurto <my.mvplace@gmail.com>
2025-10-01 15:58:31 -0700

    feat: craete quiz component placeholder

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+export default function Quiz() {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswers, setUsersAnswers] = useState([]);
+  return <p>Currently active Questions</p>;
+}
