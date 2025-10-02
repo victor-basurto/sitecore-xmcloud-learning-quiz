f474f28b2fd4d5c33f4c4e7f0843f4eb2c6281c1
Victor Basurto <my.mvplace@gmail.com>
2025-10-02 11:46:55 -0700

    feat: highlight selected answer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,37 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 import QUESTIONS from "../data/mock-qa.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
 export default function Quiz() {
+  const shuffledAnswers = useRef();
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUsersAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
   /**
    * adds selected answer from user into `userAnswers` array.
    * @param {string} selectedAnswer
    * */
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer,
-  ) {
-    setUsersAnswers((prevUserAnswers) => {
-      return [...prevUserAnswers, selectedAnswer];
-    });
-  }, []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAnswerState(selectedAnswer);
+      setUsersAnswers((prevUserAnswers) => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [setAnswerState],
+  );
   // useCallback since this function dependss on props and state
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -32,10 +47,12 @@ export default function Quiz() {
       </div>
     );
   }
-  // shuffleAnswers should run after `quizIsComplete` to confirm it doesn't break
-  // on the first iteration. This make sure it still have questions to display
-  const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffleAnswers.sort(() => Math.random() - 0.5);
+  if (!shuffledAnswers.current) {
+    // shuffleAnswers should run after `quizIsComplete` to confirm it doesn't break
+    // on the first iteration. This make sure it still have questions to display
+    shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
   return (
     <div id="quiz">
       <div id="question">
@@ -47,13 +64,29 @@ export default function Quiz() {
         />
         <p>{QUESTIONS[activeQuestionIndex].text}</p>
         <ul id="answers">
-          {shuffleAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffledAnswers.current.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let cssClass = "";
+            if (answerState === "answered" && isSelected) {
+              cssClass = "selected";
+            }
+            if (
+              (answerState === "correct" || answerState === "wrong") &&
+              isSelected
+            ) {
+              cssClass = answerState;
+            }
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={cssClass}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
