a1d988de8b8df32232149efd95f15e67e866d1a6
Victor Basurto <my.mvplace@gmail.com>
2025-10-01 23:01:06 -0700

    feat: component feed mock data simple

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,27 @@
 import { useState } from "react";
+import QUESTIONS from "../data/mock-qa.js";
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUsersAnswers] = useState([]);
-  return <p>Currently active Questions</p>;
+  const activeQuestionIndex = userAnswers.length;
+  function handleSelectAnswer(selectedAnswer) {
+    setUsersAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+  return (
+    <div id="quiz">
+      <div id="question">
+        <p>{QUESTIONS[activeQuestionIndex].text}</p>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className={"answer"}>
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
